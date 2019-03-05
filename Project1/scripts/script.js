cc95624eebaa100fb6161decfb8a4b57e430636f
Mário Santos <marioassantos@.github.com>
2019-03-05 07:13:07 +0000

    Teste git editando ficheiro html

---
+++ Project1/scripts/script.js
@@ -0,0 +1,9 @@
+var a = 10;
+var b = 20;
+
+if (a < b) {
+    var result = a + b;
+    document.write("the result of a and b is:", result);
+} else {
+    document.write("the data is not valid");
+}
